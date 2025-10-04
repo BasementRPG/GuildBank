0a32f1a25230e5ea728483272b3eb5c287bee9ec
BasementRPG <107715049+BasementRPG@users.noreply.github.com>
2025-10-03 23:30:36 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -285,7 +285,7 @@ async def view_bank(interaction: discord.Interaction):
         embed.add_field(
             name=row["name"],
             value=(
-                f"Type: {row['type']} | {row['subtype']} | Attack/Delay: {row['stats']}
+                f"Type: {row['type']} | {row['subtype']} | Attack/Delay: {row['stats']}"
                 f"  {row['stats']}"
                 f" Classes: {classes_sorted}\n"
             ),
@@ -318,6 +318,7 @@ async def remove_item(interaction: discord.Interaction, item_name: str):
     await interaction.response.send_message(f"🗑️ Deleted **{item_name}** from the Guild Bank.", ephemeral=True)
 
 bot.run(TOKEN)
+
 
 
 
