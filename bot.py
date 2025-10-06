58c23b4a9b095fb3093266e3b20b7035975e9b5e
BasementRPG <107715049+BasementRPG@users.noreply.github.com>
2025-10-06 16:19:42 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ db_pool: asyncpg.Pool = None
 
 # ---------- DB Helpers ----------
 
-async def add_item_db(guild_id, name, type_, subtype=None, stats=None, classes=None, image=None, donated_by=None):
+async def add_item_db(guild_id, name, type_, subtype=None, stats=None, classes=None, image=None, donated_by=None, qty=None):
     async with db_pool.acquire() as conn:
         await conn.execute('''
             INSERT INTO inventory (guild_id, name, type, subtype, stats, classes, image, donated_by, qty)
